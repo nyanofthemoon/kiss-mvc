c03f4c11634f484d43b4722e44ad708d5dbf603c
NyanOfTheMoon <nyanofthemoon@gmail.com>
2015-08-20 17:30:28 -0400

    initial commit

---
+++ system/lib/Cache.php
@@ -0,0 +1,42 @@
+﻿<?php
+
+class Cache 
+{
+
+	// CLASS OBJECT CACHES
+	const USER = 'user';
+	
+    public static function fetch( $cache, $id = null )
+    {
+    	$filepath = self::getCacheFilepath( $cache, $id );
+		if ( file_exists( $filepath ) )
+		{
+			return unserialize( Loader::file( $filepath ) );
+		}
+		return null;
+    }
+	
+    public static function store( $cache, $object, $id = null )
+    {
+    	return @file_put_contents( self::getCacheFilepath( $cache, $id ), serialize( $object ) );
+    }
+    
+    private static function getCacheFilepath( $cache, $id = null )
+    {
+    	if ( is_null( $id ) )
+    	{
+    		return CAC . $cache . '.ser';
+    	}
+    	else
+    	{
+    		return CAC . $cache . '/' . $id . '.ser';
+    	}
+    }
+    
+    public static function hasCache( $cache )
+    {
+    	$reflection = new ReflectionClass( 'Cache' );
+    	return in_array( $cache, $reflection->getConstants() );
+    }
+    
+}
